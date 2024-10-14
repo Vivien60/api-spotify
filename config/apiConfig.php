a2d2cdda2e95beffdf7c70004c3008ae54920da6
Vivien60 <phedras.ch@gmail.com>
2024-10-14 20:14:20 +0200

    Test de commit

---
+++ config/apiConfig.php
@@ -0,0 +1,9 @@
+<?php
+const API_BASE_URL = 'https://api.spotify.com/v1/';
+const TOKEN_BASE_URL = 'https://accounts.spotify.com/';
+const CLIENT_ID = '___';
+const CLIENT_SECRET = '___';
+const REDIRECT_URI = '___';
+const SCOPE = 'user-library-modify app-remote-control playlist-modify-private playlist-modify-public playlist-read-collaborative playlist-read-private streaming user-follow-read user-library-read user-modify-playback-state user-read-currently-playing user-read-email user-read-playback-position user-read-playback-state user-read-private user-read-recently-played user-top-read';
+const LYRICS_API_BASE_URL = 'https://api.lyrics.ovh/v1/';
+define('TOKEN_STORAGE_FILE', dirname(__FILE__,2).DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'token.json');
