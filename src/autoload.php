25cb338d4e6020d942f65e4530c4e4c2b8ff1a01
Vivien60 <phedras.ch@gmail.com>
2024-12-26 20:32:32 +0100

    - to better mocking ( for test use ), autoload function has been split, so parts can be unregistered, for example to change conf folder for test.

--- src/autoload.php
+++ src/autoload.php
@@ -1,22 +1,35 @@
 <?php
-function autoload1247575(string $nomClasse) : void {
-    $class = implode(DIRECTORY_SEPARATOR, [
-        "..",
+function autoload_apiSpotify_1247575(string $nomClasse) : void {
+    $srcClass = implode(DIRECTORY_SEPARATOR, [
+        dirname(__DIR__),
         "src",
+        str_replace("\\", DIRECTORY_SEPARATOR, "$nomClasse.php"),
+    ]);
+    if(file_exists($srcClass)) {
+        include $srcClass;
+        return;
+    }
+}
+
+/**
+ * @param string $nomClasse
+ * @return string
+ */
+function configFiles_1247575(string $nomClasse): void
+{
+    if(!(stripos($nomClasse, 'config') !== false)) {
+        return;
+    }
+    $class = implode(DIRECTORY_SEPARATOR, [
+        dirname(__DIR__),
         str_replace("\\", DIRECTORY_SEPARATOR, "$nomClasse.php"),
     ]);
     if(file_exists($class)) {
         include $class;
         return;
     }
-    $class = implode(DIRECTORY_SEPARATOR, [
-        "..",
-        str_replace("\\", DIRECTORY_SEPARATOR, "$nomClasse.php"),
-    ]);
-    include $class;
-
 }
-
-spl_autoload_register('autoload1247575');
+spl_autoload_register('autoload_apiSpotify_1247575');
+spl_autoload_register('configFiles_1247575');
 
 require_once dirname(__FILE__,2) . "/vendor/autoload.php";
