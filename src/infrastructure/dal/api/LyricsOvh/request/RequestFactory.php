aef8720d7e3bf29cd5864234e384a5d65e43696f
Vivien60 <phedras.ch@gmail.com>
2024-12-27 21:18:42 +0100

    - MusicServiceFactory has been cleaned and only methods directly related to specific api client has been kept - lyricsOvh api client has been fully implemented into MusicServiceFactory, so its RequestFactory has been created.

---
+++ src/infrastructure/dal/api/LyricsOvh/request/RequestFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace infrastructure\dal\api\LyricsOvh\request;
+
+use infrastructure\dal\api\musicService\contracts\SongRqFactoryInterface;
+use infrastructure\dal\api\RequestAbstract;
+use model\Song\Song as SongItem;
+
+class RequestFactory implements SongRqFactoryInterface
+{
+    public function lyrics(SongItem $song): RequestAbstract
+    {
+        return new Lyrics($song->artist, $song->title);
+    }
+}
--- src/infrastructure/dal/api/musicService/MusicServiceFactory.php
+++ src/infrastructure/dal/api/musicService/MusicServiceFactory.php
@@ -2,37 +2,32 @@
 
 namespace infrastructure\dal\api\musicService;
 
-use infrastructure\dal\api\Spotify\client\Client;
+use infrastructure\dal\api\LyricsOvh\client\Client as LyricsOvhCli;
+use infrastructure\dal\api\musicService\LyricsOvh\LyricsOvh;
+use infrastructure\dal\api\Spotify\client\Client as SpotifyCli;
 use infrastructure\dal\api\Spotify\client\ClientForToken;
-use infrastructure\dal\api\Spotify\request\RequestFactory;
+use infrastructure\dal\api\LyricsOvh\request\RequestFactory as LyricsOvhRequests;
+use infrastructure\dal\api\Spotify\request\RequestFactory as SpotifyRequests;
 use infrastructure\dal\api\musicService\Spotify\Spotify;
-use infrastructure\repository\contracts\MusicServiceInterface;
 use infrastructure\repository\playlist\contracts\PlaylistServiceInterface;
 use infrastructure\repository\song\contracts\SongServiceInterface;
 
 class MusicServiceFactory
 {
-    public static function default():MusicServiceInterface
-    {
-        return static::spotify();
-    }
-
-    public static function playlistService():PlaylistServiceInterface
-    {
-        return static::spotify();
-    }
-
-    public static function spotify():PlaylistServiceInterface
+    public static function spotify():PlaylistServiceInterface&OAuthInterface
     {
         return new Spotify(
-            new Client(),
-            new RequestFactory(),
+            new SpotifyCli(),
+            new SpotifyRequests(),
             new ClientForToken(),
         );
     }
 
     public static function lyricsOvh():SongServiceInterface
     {
-        return new Client();
+        return new LyricsOvh(
+            new LyricsOvhCli(),
+            new LyricsOvhRequests(),
+        );
     }
 }
