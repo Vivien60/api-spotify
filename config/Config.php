04ac1615604a80cc04ef136d06296a988bfa6601
Vivien60 <phedras.ch@gmail.com>
2025-01-01 17:04:22 +0100

    - app authentification params are now obtained from the server conf

--- config/Config.php
+++ config/Config.php
@@ -17,9 +17,6 @@ define('TOKEN_STORAGE_FILE', dirname(__FILE__,2).DIRECTORY_SEPARATOR.'data'.DIRE
 
 const API_BASE_URL = 'https://api.spotify.com/v1/';
 const TOKEN_BASE_URL = 'https://accounts.spotify.com/';
-define('CLIENT_ID', getenv("CLIENT_ID"));
-define('CLIENT_SECRET', getenv("CLIENT_SECRET"));
-const CLIENT_SECRET = '____';
 const REDIRECT_URI = '____';
 const SCOPE = 'user-library-modify app-remote-control playlist-modify-private playlist-modify-public playlist-read-collaborative playlist-read-private streaming user-follow-read user-library-read user-modify-playback-state user-read-currently-playing user-read-email user-read-playback-position user-read-playback-state user-read-private user-read-recently-played user-top-read';
 const LYRICS_API_BASE_URL = 'https://api.lyrics.ovh/v1/';
