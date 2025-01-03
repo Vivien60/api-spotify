9c749e2cc5ebd1e0507b4d82ecacf33cc57182a0
Vivien60 <phedras.ch@gmail.com>
2025-01-03 01:59:22 +0100

    - model/domain enhancement, with method like fromArray and to Array - ApiRepos now call these methods

--- public/index.php
+++ public/index.php
@@ -2,6 +2,30 @@
 require_once "../src/autoload.php";
 session_start();
 var_dump($_SESSION);
+/**@TODO Vivien: http error mapper api. Exemple :
+ *      class ServiceAErrorMapper {
+ *          public static function mapError($response) {
+ *              switch ($response->statusCode) {
+ *                  case 401:
+ *                      throw new AuthError("Authentication failed in Service A.");
+ *                  case 404:
+ *                      throw new NotFoundError("Resource not found in Service A.");
+ *                  case 500:
+ *                      throw new ServiceAInternalError("Internal server error in Service A.");
+ *                  default:
+ *                      throw new GeneralHttpError("Unexpected error from Service A.");
+ *              }
+ *          }
+ *      }
+ */
+/**@TODO Vivien: Externalisation de la configuration :
+ *      Externalisation de la configuration dans des fichiers JSON, YAML ou une base de données
+ *      pour faciliter la gestion et les modifications des paramètres sans toucher au code.
+ */
+/**@TODO Vivien: Amélioration de la gestion des sessions et des tokens :
+ *      Vérification de la validité des tokens avant chaque utilisation
+ *      et implémentation d'un mécanisme de rafraîchissement automatique des tokens expirés.
+ */
 ?>
 <htmL lang="fr">
 <head><title>Benveniidoo !</title></head>
